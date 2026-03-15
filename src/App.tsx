c3695606e7516c7f2a4beabfa19e513be501a2d5
gavimg <gavigadagi@gmail.com>
2026-03-15 16:15:16 +0530

    Initial commit - adi-reports app

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReportsApp from './ReportsApp';
+
+const App: React.FC = () => <ReportsApp />;
+export default App;
---
+++ src/ReportsApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReportsApp: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem', background: '#f0fdf4', minHeight: '100vh' }}>
+      <h1>Reports Module</h1>
+      <p>This is the adi-reports micro-frontend.</p>
+    </div>
+  );
+};
+
+export default ReportsApp;
